5999bc5733bf0a09d0750d21875f331d309314e0
chahyoungseok <xcohdsuts@gmail.com>
2024-09-21 00:43:13 +0900

    초기 프로젝트 세팅

---
+++ src/test/java/com/example/reactive_programing/ReactiveProgramingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactive_programing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveProgramingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
